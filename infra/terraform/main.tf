9e94b2758c21e062573559100f1beedb3df2ffd0
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-09-20 22:55:25 -0300

    Implementação da Infra Secrets Manager

---
+++ infra/terraform/main.tf
@@ -0,0 +1,15 @@
+resource "aws_secretsmanager_secret" "mixfast_secrets_manager" {
+  name        = "${var.name}-secrets-manager"
+  description = "Secrets Manager da aplicação Mix Fast"
+
+  tags = var.tags
+}
+
+resource "aws_secretsmanager_secret_version" "mixfast_secrets_manager_version" {
+  secret_id     = aws_secretsmanager_secret.mixfast_secrets_manager.id
+  secret_string = <<EOF
+  {
+    "dbpassword": "q1w2e3r4"
+  }
+EOF
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "tags" {
+  type = map(string)
+  default     = {
+    "name" : "mixfast"
+    "company" : "fiap"
+  }
+}
+
+variable "name" {
+  type = string
+  default = "mixfast"
+}
